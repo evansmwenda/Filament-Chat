aa5947a727d7b13fb73974d81ecb4801bec78ce1
evansmwenda <evansmwenda.em@gmail.com>
2024-05-25 22:16:13 +0300

    add chat page

---
+++ app/Filament/Pages/ChatPage.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Page;
+
+class ChatPage extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.pages.chat-page';
+
+    protected static ?string $navigationLabel = 'Chats';
+
+    protected static ?string $slug = 'chats';
+}
---
+++ resources/views/filament/pages/chat-page.blade.php
@@ -0,0 +1,15 @@
+<x-filament::page>
+    <div class="flex h-full" style="background-color: red;">
+        <!-- Contacts List -->
+        <div class="w-1/4  border-r h-full">
+           
+            <p>conversation list here</p>
+        </div>
+        
+        <!-- Chat Window -->
+        <div class="w-3/4 h-full">
+            
+            <p>chat window here</p>
+        </div>
+    </div>
+</x-filament::page>
