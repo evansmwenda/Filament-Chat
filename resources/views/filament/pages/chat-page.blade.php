e504e814e5f3dbed98abf640e6c64cc1f2211919
evansmwenda <evansmwenda.em@gmail.com>
2024-05-26 14:32:21 +0300

    has style bug

--- resources/views/filament/pages/chat-page.blade.php
+++ resources/views/filament/pages/chat-page.blade.php
@@ -1,12 +1,16 @@
 <x-filament::page>
-<div class="flex h-full">
-        <div class="w-1/4 border-r">
+    <div class="flex h-screen lg:h-[90%] bg-red-500 lg:shadow-sm overflow-hidden lg:top-16 lg:inset-x-2 m-auto rounded-t-lg">
+        <div class="w-1/3 border-r relative md:w-[320px] xl:w-[400px] overflow-y-auto shrink-0 h-full">
             <livewire:ChatList/>
         </div>
-        
-        <div class="w-3/4">
+        <div class="w-2/3 h-full relative overflow-y-auto" style="contain:content">
             <livewire:ChatWindow/>
         </div>
     </div>
 </x-filament::page>
+
+
+
+
+
 
--- resources/views/livewire/chat-window.blade.php
+++ resources/views/livewire/chat-window.blade.php
@@ -1,16 +1,18 @@
-<div>
+<div class="flex flex-col bg-red-500">
     @if($conversation)
-        <div class="messages">
+        <div class="flex-1 messages p-4">
             @foreach($messages as $message)
                 <div>{{ $message->message }}</div>
             @endforeach
         </div>
-        <div class="send-message">
-            <input type="text" wire:model="messageText" />
-            <button wire:click="sendMessage">Send</button>
+        <div class="send-message p-4 flex-none">
+            <input type="text" wire:model="messageText" class="w-3/4 p-2 border border-gray-300 rounded" />
+            <button wire:click="sendMessage" class="w-1/4 p-2 bg-blue-500 text-white rounded">Send</button>
         </div>
     @else
-        <div><h4 class="font-medium text-lg"> Choose a conversation to start chatting </h4></div>
+        <div class="flex items-center justify-center h-full p-4">
+            <h4 class="font-medium text-lg text-green-500">Choose a conversation to start chatting</h4>
+        </div>
     @endif
 </div>
 
