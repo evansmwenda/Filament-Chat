54c75fd10ed8439ef054a155588bf7b0b25c565b
evansmwenda <evansmwenda.em@gmail.com>
2024-05-27 16:35:46 +0300

    update tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,34 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import preset from './vendor/filament/support/tailwind.config.preset'
+
+
 /** @type {import('tailwindcss').Config} */
 export default {
+  presets: [preset],
   content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+    './vendor/filament/**/*.blade.php',
+
+    
+    './resources/views/**/*.blade.php',
+    './resources/js/**/*.vue',
+    './resources/css/**/*.css',
+    './app/Http/Livewire/**/*.php',
+    './vendor/filament/**/*.blade.php', 
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+        fontFamily: {
+            sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+        },
+    },
+},
+
+plugins: [
+  require('@tailwindcss/forms'),
+  require('@tailwindcss/typography'),
+],
 }
 
