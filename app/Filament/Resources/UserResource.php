76a691ac245d38970cddd480f25e02235cf6069b
evansmwenda <evansmwenda.em@gmail.com>
2024-05-28 10:25:15 +0300

    added user resource

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -6,9 +6,14 @@ use App\Filament\Resources\UserResource\Pages;
 use App\Filament\Resources\UserResource\RelationManagers;
 use App\Models\User;
 use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -17,13 +22,23 @@ class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    // protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?int $navigationSort = 2;
+
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                Section::make('User Details')
+                ->schema([
+                    TextInput::make('name')->required(),
+                    TextInput::make('email')->email()->required(),
+                    TextInput::make('password')->password()->required(),
+                    FileUpload::make('image')
+                    ->image()
+                    ->imageEditor()
+                ])->columns(2)
             ]);
     }
 
@@ -31,7 +46,12 @@ class UserResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('name')->searchable(),
+                TextColumn::make('email')
+                ->color('gray')
+                ->limit(50)
+                ->searchable(),   
+                ImageColumn::make('image')
             ])
             ->filters([
                 //
